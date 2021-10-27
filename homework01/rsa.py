ddc79b9a10fee0aab737baafc9a2820310108018
sevruk-artyom <artyom.sevruk03@gmail.com>
2021-10-27 11:08:13 +0500

    Реализованы функции encrypt_rsa и decrypt_rsa

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -5,7 +5,6 @@ import typing as tp
 def is_prime(n: int) -> bool:
     """
     Tests to see if a number is prime.
-
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -13,33 +12,65 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    k = 0
+    for i in range(1, round(n ** (1 / 2)) + 1):
+        if n % i == 0:
+            k += 1
+    if n == 2:
+        n = bool(True)
+    elif n == 1:
+        n = bool(False)
+    else:
+        if k > 1:
+            n = bool(False)
+        else:
+            n = bool(True)
+    return n
 
 
 def gcd(a: int, b: int) -> int:
     """
     Euclid's algorithm for determining the greatest common divisor.
-
     >>> gcd(12, 15)
     3
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    c = min(a, b)
+    m = 1
+    for i in range(1, c + 1):
+        if a % i == 0 and b % i == 0:
+            if i > m:
+                m = i
+    if a == 0 or b == 0:
+        m = max(a, b)
+    if a == 0 and b == 0:
+        m = 0
+    return m
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
     """
     Euclid's extended algorithm for finding the multiplicative
     inverse of two numbers.
-
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+    delen: list = []
+    phi1 = phi
+    delen.insert(0, phi // e)
+    while phi % e != 0:
+        c = phi % e
+        phi = e
+        e = c
+        delen.insert(0, phi // e)
+    x = 0
+    y = 1
+    for i in range(1, len(delen)):
+        x1 = y
+        y = x - x1 * delen[i]
+        x = x1
+    return y % phi1
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -49,10 +80,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
