854b8f575b17b635d2929cde81beb4e4a86f21d7
sevruk-artyom <90193058+sevruk-artyom@users.noreply.github.com>
2022-01-21 19:11:26 +0300

    Update maze_gui.py

--- homework04_new/maze_gui.py
+++ homework04_new/maze_gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
-from typing import List
+from typing import List, Union
 
 from maze import add_path_to_grid, bin_tree_maze, solve_maze
 
@@ -13,7 +13,7 @@ def draw_cell(x, y, color, size: int = 10):
     canvas.create_rectangle(x, y, x1, y1, fill=color)
 
 
-def draw_maze(grid: List[List[str]], size: int = 10):
+def draw_maze(grid: List[List[Union[str, int]]], size: int = 10):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == " ":
